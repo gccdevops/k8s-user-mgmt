acc9fe48f6edbcbe52399fb916b090d3332c63d6
chao.qiao <chao.m.qiao@foxmail.com>
2023-04-02 17:21:27 +0800

    fix csr api version and multi context

--- create-user.sh
+++ create-user.sh
@@ -13,11 +13,12 @@ openssl req -new -newkey rsa:4096 -nodes -keyout $USERNAME-k8s.key -out $USERNAM
 echo "upload CSR to Kubernetes"
 
 cat <<EOT | kubectl apply -f -
-apiVersion: certificates.k8s.io/v1beta1
+apiVersion: certificates.k8s.io/v1
 kind: CertificateSigningRequest
 metadata:
   name: $USERNAME-k8s-access
 spec:
+  signerName: kubernetes.io/kube-apiserver-client  
   groups:
   - system:authenticated
   request: $( cat $USERNAME-k8s.csr | base64 | tr -d '\n' )
@@ -33,15 +34,25 @@ echo "retrieve signed certificate from Kubernetes"
 
 kubectl get csr $USERNAME-k8s-access -o jsonpath='{.status.certificate}' | base64 --decode > $USERNAME-k8s-access.crt
 
+echo "retrieve current context"
+CURRENT_CONTEXT=$(kubectl config current-context)
+CURRENT_CLUSTER_NAME=$(kubectl config view -o jsonpath="{.contexts[?(@.name==\"$CURRENT_CONTEXT\")].context.cluster}")
+CURRENT_CLUSTER_SERVER=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"$CURRENT_CLUSTER_NAME\")].cluster.server}")
+
 echo "retrieve Kubernetes CA"
 
-kubectl config view -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' --raw | base64 --decode - > k8s-ca.crt
+# kubectl config view -o jsonpath='{.clusters[0].cluster.certificate-authority-data}' --raw | base64 --decode - > k8s-ca.crt
+kubectl config view -o jsonpath="{.clusters[?(@.name==\"$CURRENT_CLUSTER_NAME\")].cluster.certificate-authority-data}" --raw | base64 -d > k8s-ca.crt
 
 echo "create kubeconfig for user $USERNAME"
 
-kubectl config set-cluster $(kubectl config view -o jsonpath='{.clusters[0].name}') --server=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}') --certificate-authority=k8s-ca.crt --kubeconfig=$USERNAME-k8s-config --embed-certs
+# kubectl config set-cluster $(kubectl config view -o jsonpath='{.clusters[0].name}') --server=$(kubectl config view -o jsonpath='{.clusters[0].cluster.server}') --certificate-authority=k8s-ca.crt --kubeconfig=$USERNAME-k8s-config --embed-certs
+kubectl config set-cluster $CURRENT_CLUSTER_NAME --server=$CURRENT_CLUSTER_SERVER --certificate-authority=k8s-ca.crt --kubeconfig=$USERNAME-k8s-config --embed-certs
+
 kubectl config set-credentials $USERNAME --client-certificate=$USERNAME-k8s-access.crt --client-key=$USERNAME-k8s.key --embed-certs --kubeconfig=$USERNAME-k8s-config
-kubectl config set-context $USERNAME --cluster=$(kubectl config view -o jsonpath='{.clusters[0].name}') --namespace=$NAMESPACE --user=$USERNAME --kubeconfig=$USERNAME-k8s-config
+
+#kubectl config set-context $USERNAME --cluster=$(kubectl config view -o jsonpath='{.clusters[0].name}') --namespace=$NAMESPACE --user=$USERNAME --kubeconfig=$USERNAME-k8s-config
+kubectl config set-context $USERNAME --cluster=$CURRENT_CLUSTER_NAME --namespace=$NAMESPACE --user=$USERNAME --kubeconfig=$USERNAME-k8s-config
 kubectl config use-context $USERNAME --kubeconfig=$USERNAME-k8s-config
 
 echo "create rolebinding for user $USERNAME"
